e2feb8e87cf489ffb517b7a2a7872e80cfa365d0
dluze <dluzewskiantoni@gmail.com>
2025-07-14 16:34:07 +0200

    Komunikacja iframe > host

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('host-app');
+  @HostListener('window:message', ['$event'])
+  handleButtonClicked(event: MessageEvent) {
+    if (event.data?.type === 'buttonClicked') {
+      console.log('buttonClicked', event.data.detail);
+    }
+  }
 }
